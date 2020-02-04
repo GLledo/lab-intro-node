599210983cf18056104d147de8f1a6344da240cf
Gonzalo lledo <gonzalo_lledo@outlook.com>
2020-02-04 13:08:32 +0100

    Subir

--- starter-code/index.js
+++ starter-code/index.js
@@ -11,35 +11,23 @@ class SortedList {
   }
 
   get(pos) {
-    if (pos < this.length){
-      return this.items[pos]
-    }else{
-      throw new Error("OutOfBounds");
-    }
+    if (pos < this.length) return this.items[pos]
+    else throw new Error("OutOfBounds");
+    
   }
 
   max() {
-    if (this.length != 0){
-      return this.items[this.length-1]
-    }else{
-      throw new Error("EmptySortedList")
-    }
+    if (this.length != 0) return this.items[this.length-1]
+    else throw new Error("EmptySortedList")
   }
 
   min() {
-    if (this.length != 0){
-      return this.items[0]
-    }else{
-      throw new Error("EmptySortedList")
-    }
+    if (this.length != 0) return this.items[0]
+    else throw new Error("EmptySortedList")
   }
 
   sum() {
-    if (this.length != 0){
-      return this.items.reduce((acc, sum) => acc + sum)
-    }else{
-      return 0
-    }
+      return this.length != 0 ?this.items.reduce((acc, sum) => acc + sum): 0
   }
 
   avg() {
